aec68e7d960e4dc3db49a7630cc562dbc404f206
Milica Maric <milicamaric321@gmail.com>
2021-02-14 01:38:55 +0100

    dodat react ui, eksperimentalno

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,7 +1,15 @@
 <html>
 
+<head>
+    <!-- ne treba kad se ukljuci <script src="{{ asset('js/app.js') }}"></script> na kraj body-ja -->
+    <!--<script type="application/javascript" src="/js/app.js"></script>-->
+</head>
+
 <body>
     <h1> tesssst</h1>
+    <div id="example"></div>
+    <div id="index"></div>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 
 </html>
